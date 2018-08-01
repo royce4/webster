33c73685e0f4a25c92c7369edfc9aab36534d38d
royce4 <42007998+royce4@users.noreply.github.com>
2018-08-01 13:02:19 -0500

    website files
    website files for royce4.net

---
+++ script.js
@@ -0,0 +1,10 @@
+            $(document).ready(function(){
+
+                $(".msg").fadeIn(2000);
+                $("#aboutMe").click(function(){
+                    window.location = 'aboutme.html';
+                    });
+                $(".initials").click(function(){
+                    window.location = 'index.html';
+                    });    
+                });
